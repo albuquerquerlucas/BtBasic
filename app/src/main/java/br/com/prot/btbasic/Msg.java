ec73478298f3915fa77c789ada9501f5f9bdfa55
Lucas [IVIA] <lucas.abuquerque@iviatecnologia.com.br>
2018-03-23 16:41:04 -0300

    Classe de Mensagens

--- app/src/main/java/br/com/prot/btbasic/Msg.java
+++ app/src/main/java/br/com/prot/btbasic/Msg.java
@@ -7,4 +7,10 @@ package br.com.prot.btbasic;
 public class Msg {
 
     public static final String SUPORTE_BLUETOOTH = "Não suportado.";
+    public static final String AGUARDANDO_USUARIO = "Aguardando confirmação do usuário...";
+    public static final String BLUETOOTH_ATIVO = "Bluetooth ativo.";
+    public static final String BLUETOOTH_ON = "ON";
+    public static final String BLUETOOTH_OFF = "OFF";
+    public static final String DISPOSITIVOS_PAREADOS = "Dispositivos Pareados.";
+    public static final String DISPOSITIVOS_PROXIMOS = "Dispositivos Encontrados.";
 }
